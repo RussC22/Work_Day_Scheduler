a4edf7979e2c4ede84379e225b8bf475b4906e10
RussC22 <114961971+RussC22@users.noreply.github.com>
2022-11-25 02:47:02 -0500

    ReadMeUpdate

--- README_Blank.md
+++ README_Blank.md
@@ -1,1 +1,29 @@
 # Work_Day_Scheduler
+
+This weeks activity centers around creating a simple calendar application that allows a user to save events for each hour of the day by modifying starter code.
+
+## Technologies
+
+- VS code
+- Github
+- Gitlab
+- jQuery
+- CSS
+- HTML
+- javaScript
+- tailwindcss
+- getbootstrap
+- googleFont
+- day.js.org
+
+## Usage
+
+This is a everyday application that can organized the most complicated individuals schedule.
+
+## Contributing
+
+n/a
+
+## License
+
+n/a
--- script.js
+++ script.js
@@ -31,25 +31,7 @@ $('.time-block').each(function () {
     if (currentHour > hour ) {$(this).addClass('future');}
     if (currentHour === hour ) {$(this).addClass('present');}
 });
-  
-
-  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? 
-  
-  
-
-
-  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
-  
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
-  
-
-
-  // TODO: Add code to display the current date in the header of the page.
-
-
-// });
+// save button event function
 function saveToLs(event) {  
   // console.dir (this)
   var value= $(this).siblings('.description').val()
@@ -60,13 +42,11 @@ function saveToLs(event) {
     allTimes[time]=value
   localStorage.setItem('time',JSON.stringify(allTimes))
 }
-
+// 
 function loadFromLs() { 
   var allTimes=JSON.parse(localStorage.getItem('time'))||{};
 
  }
  loadFromLs()
-//  for loop through a object: "allTimes"
-
-
+// Save button
 $(".saveBtn").on('click', saveToLs)
